0abf6fccf1671a5aee78f09c9ff020d5f4b9852a
Dawit Mekonnen <56738450+Dave-lab12@users.noreply.github.com>
2024-10-12 14:50:11 +0300

    added campaign

---
+++ src/routes/campaign/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	import { marked } from 'marked';
+	import content from './content.md?raw';
+
+	export let data: PageData;
+</script>
+
+<!-- <img src="/campaign/banner-1.png" alt="" /> -->
+<div class="prose prose-invert prose-a:text-cyan-700 lg:prose-xl mx-auto mb-10">
+	{@html marked(content)}
+</div>
